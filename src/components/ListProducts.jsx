117f9b65a18a4e5e5ab3f61620b574d70cfdca0e
Ekin Utku <ekinutkugulmez@gmail.com>
2024-05-14 13:07:53 +0300

    products page

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
+import product_card from "../data/product-data";
+import '../css/ListProduct.css';
 
 
 class ListProducts extends Component {
 
     render() {
         document.title = "Products";
+
+        console.log(product_card);
+
+        const listItems = product_card.map((item) =>
+            <div className="card" key={item.id}>
+                <div className="card_img">
+                    <img src={item.thumb} />
+                </div>
+                <div className="card_header">
+                    <h2>{item.product_name}</h2>
+                    <p>{item.description}</p>
+                    <p className="price">{item.price}<span>{item.currency}</span></p>
+                    <div className="addToCard">Add to cart</div>
+                </div>
+            </div>
+        );
+
         return (
-            <h1>Here you can see the products</h1>
+            <div className="wrapper">
+                <div className="main_content">
+                    {listItems}
+                </div>
+            </div>
         )
     }
 }
