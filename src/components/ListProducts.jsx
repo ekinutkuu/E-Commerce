1501bd1ad0e59b9385c217da09984e54c49e975a
Ekin Utku <ekinutkugulmez@gmail.com>
2024-06-26 10:27:49 +0300

    product page update

--- src/components/ListProducts.jsx
+++ src/components/ListProducts.jsx
@@ -21,56 +21,33 @@ class ListProducts extends Component {
 
     render() {
         document.title = "Products";
-
-        console.log(product_card);
-
-        const listItems = product_card.map((item) =>
-            <div className="card" key={item.id}>
-                <div className="card_img">
-                    <img src={item.thumb} alt='product' />
-                </div>
-                <div className="card_header">
-                    <h2>{item.product_name}</h2>
-                    <p>{item.description}</p>
-                    <p className="price">{item.price}<span>{item.currency}</span></p>
-                    <div className="addToCard">Add to cart</div>
-                </div>
-            </div>
-        );
-
         return (
-/*             <div className="wrapper">
-                <div className="main_content">
-                    {listItems}
-                </div> */
-
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th> Product ID </th>
-                                <th> Product Name </th>
-                                <th> Product Price </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                this.state.products.map(
-                                    product => 
-                                    <tr key={product.productId}>
-                                        <td> {product.productId} </td>
-                                        <td> {product.productName}</td>
-                                        <td> {product.productPrice}</td>
-                                        <td>
-                                            <button onClick={() => this.edituser(product.id)} className="btn btn-info">Update </button>
-                                            <button style={{marginLeft: "10px"}} onClick={() => this.deleteuser(product.producId)} className="btn btn-danger">Delete </button>
-                                            <button style={{marginLeft: "10px"}} onClick={() => this.viewuser(product.producId)} className="btn btn-info">View </button>
-                                        </td>
-                                    </tr>
-                                )
-                            }
-                        </tbody>
-                    </table>
+                <div className="wrapper">
+                    <div className="main_content">
+                        <table className="product-table">
+                            <thead>
+                                <tr>
+                                    {/* <th> Product ID </th> */}
+                                    <th> Product Name </th>
+                                    <th> Product Price </th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.products.map(
+                                        product => 
+                                        <tr>
+                                            {/* <td> {product.productId} </td> */}
+                                            <td> {product.productName}</td>
+                                            <td> {product.productPrice}</td>
+                                            <td><button class="add-to-cart">Add to Cart</button></td>
+                                        </tr>
+                                    )
+                                }
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
         );
     }
